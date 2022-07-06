353aed9e485679660c53da57abdb16e520075bd1
leftover <1614674987@qq.com>
2022-07-06 22:46:35 +0800

    feat: 获取不同环境变量

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,10 @@
-import dotenv from 'dotenv'
+import dotenv, { DotenvParseOutput } from 'dotenv'
+import path from 'path'
 
-console.log(dotenv.config)
+function getEnv(filename: string): DotenvParseOutput | undefined {
+  const envPath = path.resolve(process.cwd(), filename)
+  return dotenv.config({ path: envPath }).parsed
+}
+export const baseEnv = getEnv('.env')
+export const devEnv = getEnv('.development.env')
+export const prodEnv = getEnv('.production.env')
