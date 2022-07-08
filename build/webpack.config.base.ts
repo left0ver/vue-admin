8d3426efe7640b637ad8a5dd87047c1a886fefd9
leftover <1614674987@qq.com>
2022-07-08 20:36:51 +0800

    chore: add DllReferencePlugin

--- build/webpack.config.base.ts
+++ build/webpack.config.base.ts
@@ -4,6 +4,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { VueLoaderPlugin } from 'vue-loader'
 import ElementPlus from 'unplugin-element-plus/webpack'
 
+const dllPath = 'public/dll'
 const baseConfig: webpack.Configuration = {
   entry: path.resolve(__dirname, '../src', 'main.ts'),
   // target: 'browserslist',
@@ -100,6 +101,9 @@ const baseConfig: webpack.Configuration = {
     new VueLoaderPlugin(),
     // 显示打包进度
     new ProgressPlugin(),
+    new webpack.DllReferencePlugin({
+      manifest: path.resolve(__dirname, '../', dllPath, 'vue-manifest.json'),
+    }),
   ],
 }
 
