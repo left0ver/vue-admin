57bcf0b3926389dde563f377ffeb9a1c544a705d
leftover <1614674987@qq.com>
2022-07-31 09:53:52 +0800

    feat:reset函数清除pinia中的数据

--- src/utils/reset.ts
+++ src/utils/reset.ts
@@ -1,8 +1,20 @@
+import type { StoreDefinition } from 'pinia'
 import { removeRefreshToken, removeAccessToken } from '@/utils/auth'
+import * as rootStore from '@/store'
 
+type StoreKeys = keyof typeof rootStore
 export function reset(): void {
   removeAccessToken()
   removeRefreshToken()
   // 清除pinia存在sessionStorage中的数据
   sessionStorage.clear()
+  const storeKeys = Object.keys(rootStore) as StoreKeys[]
+  // 清除pinia中的数据
+  storeKeys.forEach(key => {
+    if (Object.prototype.hasOwnProperty.call(rootStore, key)) {
+      const useStore = rootStore[key] as StoreDefinition
+      const store = useStore()
+      store.$reset()
+    }
+  })
 }
